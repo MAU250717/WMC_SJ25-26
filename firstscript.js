0772ce8ffc856b215ab6761bd63c3ad1022f5c54
MAU250717 <MAU250717@spengergasse.at>
2025-10-23 10:44:19 +0200

    schöner

--- firstscript.js
+++ firstscript.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 function zeigeMehr() {
       document.getElementById("mehrText").style.display = "block";
-=======
-function zeigeMehr() {
-      document.getElementById("mehrText").style.display = "block";
->>>>>>> 78dc0eb017e101810cdf9e08db8a94719f3ed005
+      document.getElementById("mehrButton").style.display = "none";
     }
